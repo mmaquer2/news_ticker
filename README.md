b2122ee068bdd5f1535062efd4f59f2db0bcedee
unknown <mike.maquera212@gmail.com>
2019-08-13 19:30:53 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+##News Ticker App
+
+- News ticker App desktop application made using electron.js 
+- add various news source headlines, stocks, sports news to a horizontal moving news ticker
+
+- API's used include Alpha Vantage (stocks/finance),  news api .org (headlines), TBD (sports)
---
+++ app.js
@@ -0,0 +1,36 @@
+// sources for of APIs
+    //-Api news (headlines)
+    //alpha vantage (stocks)
+    //espn (sports)
+
+    function test() {
+        console.log('hello world')
+        
+
+    }
+
+    const alpha_key = ('YIF9NU7FLVJWG46Q');
+    const url = ('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=MSFT&apikey=YIF9NU7FLVJWG46Q');
+    async function test_data() {
+
+        const response =await fetch(url);
+        const data = await response.json();
+
+
+        console.log(data)
+
+    }
+
+
+    const news_key = ('a0797ceb9fc0435c9e7e691760ad4244')
+    const news_url = ('https://newsapi.org/v2/top-headlines?country=us&apiKey=a0797ceb9fc0435c9e7e691760ad4244')
+    async function test_news_api () {
+
+        const response = await fetch(news_url);
+        const news_data = await response.json();
+
+        console.log (news_data)
+    }
+
+
+  
